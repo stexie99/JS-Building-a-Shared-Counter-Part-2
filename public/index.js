bacc04dcf225d0828eee6342c709b479b1c65dfa
Steven Xie <xies1999@hotmail.com>
2024-03-06 14:51:37 -0500

    added reset button

--- public/index.js
+++ public/index.js
@@ -2,6 +2,7 @@ async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+    const resetButton = document.querySelector('#reset-button')
 
     const response = await fetch('http://localhost:9001/counter');
 
@@ -37,8 +38,23 @@ async function main(){
         })
     }
 
+    function reset(){
+        countValue= 10
+        countContainer.textContent = countValue
+        fetch('http://localhost:9001/counter',{
+            method: 'PATCH',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({
+                value: countValue
+            })
+        })
+    }
+
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    resetButton.addEventListener('click', reset)
     countContainer.textContent = countValue;
 }
 main()
